abfbd1e958921d2ef009fe15c01ac840f32baf37
antoniosanp <antonio.pulgarin97@gmail.com>
2026-03-31 13:44:53 -0500

    feature: opciones menu

--- menujava/src/main/java/org/example/Menu.java
+++ menujava/src/main/java/org/example/Menu.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Menu {
+
+    public static void opcionAgregarEstudiante(ListaEstudiantes lista, Estudiante e, Scanner sc){
+
+        String nombre = ValidateData.validateString("Nombre del estudiante: ", sc);
+        int edad = ValidateData.validateEdad(sc);
+
+        Estudiante estudiante = new Estudiante(nombre,edad);
+
+        lista.addEstudiante(estudiante);
+    };
+
+    public  static void opcionAgregarNotaEstudiante(ListaEstudiantes lista, Estudiante e, Scanner sc){
+        double nota = ValidateData.validateNota("nota: ",  sc);
+        lista.addNotaEstudiante(e, nota);
+    };
+
+    public static double opcionObenerPromedioEstudiante(ListaEstudiantes lista, Estudiante e){
+        return lista.calculatePromedioEstudiante(e);
+    };
+
+    public  static Estudiante opcionObenerEstudiante(ListaEstudiantes lista, String nombre){
+        return lista.encontrarEstudiante(nombre);
+    };
+
 }
